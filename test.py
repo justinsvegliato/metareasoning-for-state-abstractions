49c86ff850442e10ba2b96cb734e516283f55b94
Justin Svegliato <justin.svegliato@gmail.com>
2022-01-22 14:45:42 -0800

    Fixed a few small things - need to improve logging

--- test.py
+++ test.py
@@ -2,13 +2,12 @@ import numpy as np
 from stable_baselines3 import PPO
 from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.results_plotter import load_results, ts2xy
-from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.results_plotter import X_TIMESTEPS, load_results, ts2xy
 
 import wandb
 from metareasoning_env import MetareasoningEnv
 
-PROJECT = 'metareasoning-for-state-abstraction'
+PROJECT = 'metareasoning-for-state-abstractions'
 CONFIG = {
     'policy_type': 'MlpPolicy',
     'total_timesteps': 10000
@@ -24,13 +23,14 @@ class WandbCallback(BaseCallback):
         self.config = config
         self.logging_directory = logging_directory
 
+    def _on_training_start(self) -> None:
         self.run = wandb.init(
             project=self.project, 
             config=self.config
         )
 
     def _on_step(self) -> bool:
-        x, y = ts2xy(load_results(self.logging_directory), 'timesteps')
+        x, y = ts2xy(load_results(self.logging_directory), X_TIMESTEPS)
 
         if len(x) > 0:
             mean_reward = np.mean(y[-100:])
