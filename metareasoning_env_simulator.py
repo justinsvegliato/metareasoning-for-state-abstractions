e3022987bc7090d2f86902fc10d272cd84102d9f
Justin Svegliato <justin.svegliato@gmail.com>
2022-02-15 13:14:53 -0800

    Cleaned up and confirmed simulator code

--- metareasoning_env_simulator.py
+++ metareasoning_env_simulator.py
@@ -3,64 +3,78 @@ import numpy as np
 import seaborn as sns
 from stable_baselines3 import DQN
 
-from metareasoning_env import MetareasoningEnv, EXPANSION_STRATEGY_MAP
-
-HEATMAP_INTERVAL = 10
+from metareasoning_env import EXPANSION_STRATEGY_MAP, MetareasoningEnv
 
 # TODO Make this consistent across files
 ROWS = 2
 COLUMNS = 4
 
-ENV = MetareasoningEnv()
 
-
-def show_heatmap(decisions):
-    abstract_state_tracker = {abstract_state: {'VISITATION': 0, 'NAIVE': 0, 'PROACTIVE': 0} for abstract_state in ENV.abstract_mdp.states()}
+def show_heatmap(env, decisions, action_focus='PROACTIVE'):
+    abstract_state_tracker = {}
+    for abstract_state in env.abstract_mdp.states():
+        abstract_state_tracker[abstract_state] = {}
+        for action in EXPANSION_STRATEGY_MAP.values():
+            abstract_state_tracker[abstract_state][action] = 0
 
     for abstract_state, action in decisions:
-        abstract_state_tracker[abstract_state]['VISITATION'] += 1
         abstract_state_tracker[abstract_state][EXPANSION_STRATEGY_MAP[action]] += 1
 
-    labels = [['' for _ in range(COLUMNS)] for _ in range(ROWS)]
     heatmap_matrix = np.zeros((ROWS, COLUMNS))
+    labels = [['' for _ in range(COLUMNS)] for _ in range(ROWS)]
 
     for row in range(ROWS):
         for column in range(COLUMNS):
-            proactive_count = 0
+            count = 0
             total_count = 0
 
             location_index = row * COLUMNS + column
+
             for weather_index in range(4):
                 abstract_state = 'abstract_{}_{}'.format(location_index, weather_index)
 
-                proactive_count += abstract_state_tracker[abstract_state]['PROACTIVE'] 
-                total_count += abstract_state_tracker[abstract_state]['VISITATION']
+                count += abstract_state_tracker[abstract_state][action_focus] 
+                total_count += sum([abstract_state_tracker[abstract_state][action] for action in EXPANSION_STRATEGY_MAP.values()])
 
-                reward = ENV.abstract_mdp.reward_function(abstract_state, 'IMAGE')
+                reward = env.abstract_mdp.reward_function(abstract_state, 'IMAGE')
                 if reward > 0:
-                    labels[row][column] = 'P'
+                    labels[row][column] = 'X'
 
-            heatmap_matrix[row, column] = proactive_count / total_count if total_count > 0 else 0
+            heatmap_matrix[row, column] = count / total_count if total_count > 0 else 0
 
-    sns.heatmap(heatmap_matrix, vmin=0.0, vmax=1.0, annot=labels, square=True, linewidth=0.5, fmt='')
+    sns.set(font_scale=1.4)
+
+    sns.heatmap(heatmap_matrix, vmin=0.0, vmax=1.0, annot=labels, square=True, linewidths=1.0, cbar_kws={"orientation": "horizontal"}, fmt='')
+    
     plt.show()
 
 
 def main():
     model = DQN.load("weights/dqn")
-    decisions = []
-    step = 0
 
-    observation = ENV.reset()
+    step = 0
+    decisions = []
+
+    env = MetareasoningEnv()
+
+    observation = env.reset()
+    print("Observation:", observation)
 
     done = False
     while not done:
         action, _ = model.predict(observation)
-        observation, _, done, info = ENV.step(int(action))
-        decisions.append((info['abstract_state'], info['action']))
+
+        observation, reward, done, info = env.step(int(action))
+
+        print("Observation:", observation)
+        print("Reward:", reward)
+        print("Done:", done)
+
         step += 1
 
-    show_heatmap(decisions)
+        decisions.append((info['abstract_state'], info['action']))
+
+    show_heatmap(env, decisions)
 
         
 if __name__ == '__main__':
