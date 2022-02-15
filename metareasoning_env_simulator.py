13802399f618d0e9529253ca88d7b8772c38f9ec
Justin Svegliato <justin.svegliato@gmail.com>
2022-02-15 12:27:02 -0800

    Upgrade complete...Upgrade complete...Upgrade complete

--- metareasoning_env_simulator.py
+++ metareasoning_env_simulator.py
@@ -1,5 +1,3 @@
-import random
-
 import matplotlib.pylab as plt
 import numpy as np
 import seaborn as sns
@@ -7,14 +5,15 @@ from stable_baselines3 import DQN
 
 from metareasoning_env import MetareasoningEnv, EXPANSION_STRATEGY_MAP
 
-# # TODO Make this consistent across files
 HEATMAP_INTERVAL = 10
+
+# TODO Make this consistent across files
 ROWS = 2
 COLUMNS = 4
 
 ENV = MetareasoningEnv()
 
-# TODO Save heatmaps
+
 def show_heatmap(decisions):
     abstract_state_tracker = {abstract_state: {'VISITATION': 0, 'NAIVE': 0, 'PROACTIVE': 0} for abstract_state in ENV.abstract_mdp.states()}
 
@@ -22,7 +21,9 @@ def show_heatmap(decisions):
         abstract_state_tracker[abstract_state]['VISITATION'] += 1
         abstract_state_tracker[abstract_state][EXPANSION_STRATEGY_MAP[action]] += 1
 
+    labels = [['' for _ in range(COLUMNS)] for _ in range(ROWS)]
     heatmap_matrix = np.zeros((ROWS, COLUMNS))
+
     for row in range(ROWS):
         for column in range(COLUMNS):
             proactive_count = 0
@@ -31,18 +32,21 @@ def show_heatmap(decisions):
             location_index = row * COLUMNS + column
             for weather_index in range(4):
                 abstract_state = 'abstract_{}_{}'.format(location_index, weather_index)
+
                 proactive_count += abstract_state_tracker[abstract_state]['PROACTIVE'] 
                 total_count += abstract_state_tracker[abstract_state]['VISITATION']
 
+                reward = ENV.abstract_mdp.reward_function(abstract_state, 'IMAGE')
+                if reward > 0:
+                    labels[row][column] = 'P'
+
             heatmap_matrix[row, column] = proactive_count / total_count if total_count > 0 else 0
 
-    sns.heatmap(heatmap_matrix, linewidth=0.5)
+    sns.heatmap(heatmap_matrix, vmin=0.0, vmax=1.0, annot=labels, square=True, linewidth=0.5, fmt='')
     plt.show()
 
 
 def main():
-    random.seed(5)
-
     model = DQN.load("weights/dqn")
     decisions = []
     step = 0
@@ -52,7 +56,7 @@ def main():
     done = False
     while not done:
         action, _ = model.predict(observation)
-        observation, _, done, info = ENV.step(action)
+        observation, _, done, info = ENV.step(int(action))
         decisions.append((info['abstract_state'], info['action']))
         step += 1
 
